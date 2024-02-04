84a29952e9a4ad7980c5146e14c63f5400b58aa0
nabeel736 <158798785+nabeel736@users.noreply.github.com>
2024-02-04 12:03:59 +0530

    Update script.js

--- javascript/color_the_boxes/script.js
+++ javascript/color_the_boxes/script.js
@@ -6,7 +6,7 @@ function getRandomColor(){
     let val1 = Math.ceil(0 + Math.random()* 255);
     let val2 = Math.ceil(0 + Math.random()* 255);
     let val3 = Math.ceil(0 + Math.random()* 255);
-    return rgb(${val1}, ${val2}, ${val3})
+    return `rgb(${val1}, ${val2}, ${val3})`;
 }
 Array.from(boxes).forEach(e=>{
     e.style.backgroundColor = getRandomColor()
